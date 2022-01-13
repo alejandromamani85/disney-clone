7a7315101a7281a26e7e2d4591f96df05bfcf31c
Alejandro Mamani <alejandromamani85@gmail.com>
2022-01-13 20:32:11 -0300

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/disney-logo.png";
+import { IconHome } from "./icons";
 
-type AccountProps = {
-  username: string;
-  avatar: { url: string };
+type NavbarProps = {
+  account: {
+    username: string;
+    avatar: { url: string };
+  };
 };
 
-const Navbar = ({ account }: { account: AccountProps }) => {
+type ButtonNavbarProps = {
+  children: React.ReactNode;
+};
+
+const ButtonNavbar = ({ children }: ButtonNavbarProps) => (
+  <div className="text-sm font-semibold mx-4">{children}</div>
+);
+
+const Navbar = ({ account }: NavbarProps) => {
   return (
-    <div className="flex justify-between items-center px-8 h-20">
+    <nav className="flex justify-between items-center px-6 h-20 w-full bg-black">
       <div className="w-20">
         <Link href="/">
           <a>
@@ -17,11 +28,19 @@ const Navbar = ({ account }: { account: AccountProps }) => {
           </a>
         </Link>
       </div>
-      <div className="flex content-center items-center">
-        <span className="pr-4">Welcome {account.username} </span>
+      <div className="w-full flex px-10">
+        <ButtonNavbar>HOME</ButtonNavbar>
+        <ButtonNavbar>SEARCH</ButtonNavbar>
+        <ButtonNavbar>WATCHLIST</ButtonNavbar>
+        <ButtonNavbar>ORIGINALS</ButtonNavbar>
+        <ButtonNavbar>MOVIES</ButtonNavbar>
+        <ButtonNavbar>SERIES</ButtonNavbar>
+      </div>
+      <div className="flex shrink-0 content-center items-center">
+        <span className="pr-4 uppercase">{account.username} </span>
         <img className="w-12 h-12 rounded-full" src={account.avatar.url} />
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/pages/videos/[slug].tsx
+++ src/pages/videos/[slug].tsx
@@ -48,31 +48,31 @@ const Video: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> =
   ({ video }) => {
     const [watching, setWatching] = useState(false);
     return (
-      <>
+      <div className="text-white">
         {!watching && (
-          <img
-            className="video-image"
-            src={video.thumbnail.url}
-            alt={video.title}
-          />
-        )}
-        {!watching && (
-          <div className="info">
-            <p>{video.tags.join(", ")}</p>
-            <p>{video.description}</p>
-            <Link href="/">
-              <p>go back</p>
-            </Link>
-            <button
-              className="video-overlay"
-              onClick={() => {
-                changeToSeen(video.slug);
-                watching ? setWatching(false) : setWatching(true);
-              }}
-            >
-              PLAY
-            </button>
-          </div>
+          <>
+            <img
+              className="absolute w-full"
+              src={video.thumbnail.url}
+              alt={video.title}
+            />
+            <div className="relative top-40 left-40">
+              <p>{video.tags.join(", ")}</p>
+              <p>{video.description}</p>
+              <Link href="/">
+                <p>go back</p>
+              </Link>
+              <button
+                className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                onClick={() => {
+                  changeToSeen(video.slug);
+                  watching ? setWatching(false) : setWatching(true);
+                }}
+              >
+                PLAY
+              </button>
+            </div>
+          </>
         )}
         {watching && (
           <video width="100%" controls autoPlay={true}>
@@ -83,7 +83,7 @@ const Video: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> =
           className="info-footer"
           onClick={() => (watching ? setWatching(false) : null)}
         ></div>
-      </>
+      </div>
     );
   };
 
