f4d6e565cebc1df844f246b0b69a538ad51d2e8c
Alejandro Mamani <alejandromamani85@gmail.com>
2022-01-25 21:54:29 -0300

    Responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {
   DisneyIcon,
   HomeIcon,
+  MoreIcon,
   MoviesIcon,
   PlusIcon,
   SearchIcon,
@@ -22,81 +23,114 @@ type ButtonNavbarProps = {
 };
 
 const ButtonNavbar = ({ children }: ButtonNavbarProps) => (
-  <div className="group text-sm font-semibold p-4 flex items-center h-full">
+  <div className="group text-sm font-semibold p-3 flex items-center h-full lg:p-4">
     {children}
   </div>
 );
 
-const LinkUnderlined = ({ children }: { children: ReactNode }) => (
-  <span className="relative before:link-underline group-hover:before:link-underline-hover leading-9 rounded-sm">
+const TextUnderlined = ({ children }: { children: ReactNode }) => (
+  <span className="hidden lg:block lg:relative lg:before:link-underline lg:group-hover:before:link-underline-hover lg:leading-9 lg:rounded-sm">
     {children}
   </span>
+);
+
+const IconUnderlined = ({ children }: { children: ReactNode }) => (
+  <>
+    <span className="relative before:link-underline group-hover:before:link-underline-hover rounded-sm lg:hidden flex items-center">
+      {children}
+    </span>
+    <div className="hidden lg:block lg:mr-4">{children}</div>
+  </>
 );
 
 const Navbar = ({ account }: NavbarProps) => {
   return (
     <header className="w-full sticky top-0 z-10">
-      <nav className="flex justify-between items-stretch px-9 h-20 w-full bg-black">
-        <div className="flex items-center">
+      <nav className="flex justify-between items-stretch px-4 h-[4.5rem] w-full bg-black lg:h-20 lg:px-9">
+        <div className="flex items-center mr-6">
           <Link href="/">
             <a>
-              <DisneyIcon className="w-20" />
+              <DisneyIcon className="w-16 md:w-20" />
             </a>
           </Link>
         </div>
-        <div className="w-full flex px-10">
+        <div className="w-full flex px-0 md:px-4 lg:px-10">
           <Link href="/">
             <a>
               <ButtonNavbar>
-                <HomeIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>HOME</LinkUnderlined>
+                <IconUnderlined>
+                  <HomeIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                </IconUnderlined>
+                <TextUnderlined>HOME</TextUnderlined>
               </ButtonNavbar>
             </a>
           </Link>
           <Link href="/">
             <a>
               <ButtonNavbar>
-                <SearchIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>SEARCH</LinkUnderlined>
+                <IconUnderlined>
+                  <SearchIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                </IconUnderlined>
+                <TextUnderlined>SEARCH</TextUnderlined>
               </ButtonNavbar>
             </a>
           </Link>
           <Link href="/">
             <a>
               <ButtonNavbar>
-                <PlusIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>WATCHLIST</LinkUnderlined>
+                <IconUnderlined>
+                  <PlusIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                </IconUnderlined>
+                <TextUnderlined>WATCHLIST</TextUnderlined>
               </ButtonNavbar>
             </a>
           </Link>
-          <Link href="/">
-            <a>
-              <ButtonNavbar>
-                <StarIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>ORIGINALS</LinkUnderlined>
-              </ButtonNavbar>
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <ButtonNavbar>
-                <MoviesIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>MOVIES</LinkUnderlined>
-              </ButtonNavbar>
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              <ButtonNavbar>
-                <SeriesIcon className="w-5 h-5 mr-4" />
-                <LinkUnderlined>SERIES</LinkUnderlined>
-              </ButtonNavbar>
-            </a>
-          </Link>
+          <div className="lg:hidden">
+            <ButtonNavbar>
+              <MoreIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+              <TextUnderlined>WATCHLIST</TextUnderlined>
+            </ButtonNavbar>
+          </div>
+          <div className="hidden lg:flex">
+            <Link href="/">
+              <a>
+                <ButtonNavbar>
+                  <IconUnderlined>
+                    <StarIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                  </IconUnderlined>
+                  <TextUnderlined>ORIGINALS</TextUnderlined>
+                </ButtonNavbar>
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <ButtonNavbar>
+                  <IconUnderlined>
+                    <MoviesIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                  </IconUnderlined>
+                  <TextUnderlined>MOVIES</TextUnderlined>
+                </ButtonNavbar>
+              </a>
+            </Link>
+            <Link href="/">
+              <a>
+                <ButtonNavbar>
+                  <IconUnderlined>
+                    <SeriesIcon className="w-6 h-6 my-2 lg:w-5 lg:h-5 lg:my-0" />
+                  </IconUnderlined>
+                  <TextUnderlined>SERIES</TextUnderlined>
+                </ButtonNavbar>
+              </a>
+            </Link>
+          </div>
         </div>
-        <div className="flex shrink-0 content-center items-center">
-          <span className="pr-4 uppercase">{account.username} </span>
-          <img className="w-12 h-12 rounded-full" src={account.avatar.url} />
+        <div className="absolute right-0 px-4 py-3 md:p-4 lg:px-9">
+          <div className="flex items-center">
+            <span className="hidden xl:block pr-4 uppercase">
+              {account.username}{" "}
+            </span>
+            <img className="w-12 h-12 rounded-full" src={account.avatar.url} />
+          </div>
         </div>
       </nav>
     </header>
