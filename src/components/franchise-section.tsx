a2b3deb3009ae6213d17ec63dc6370aa4c182f19
Alejandro Mamani <alejandromamani85@gmail.com>
2025-06-23 22:21:37 +1200

    add video to brand card

--- src/components/franchise-section.tsx
+++ src/components/franchise-section.tsx
@@ -8,11 +8,11 @@ import BrandCard from "./brand-card";
 export const FranchiseSection = () => (
   <>
     <div className="hidden sm:grid sm:grid-cols-5 sm:gap-6">
-      <BrandCard src={disneyLogo} href="#disney" />
-      <BrandCard src={pixarLogo} href="#pixar" />
-      <BrandCard src={marvelLogo} href="#marvel" />
-      <BrandCard src={starwarsLogo} href="#starwars" />
-      <BrandCard src={natgeoLogo} href="#natgeo" />
+      <BrandCard src={disneyLogo} href="#disney" hoverVideo="https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217865-disney.mp4"/>
+      <BrandCard src={pixarLogo} href="#pixar" hoverVideo="https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217992-pixar.mp4"/>
+      <BrandCard src={marvelLogo} href="#marvel" hoverVideo="https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217799-marvel.mp4"/>
+      <BrandCard src={starwarsLogo} href="#starwars" hoverVideo="https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2020/12/17/1608229334-star-wars.mp4"/>
+      <BrandCard src={natgeoLogo} href="#natgeo" hoverVideo="https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217923-national-geographic.mp4"/>
     </div>
     <div className="grid grid-cols-6 gap-4 sm:hidden">
       <BrandCard src={disneyLogo} href="#disney" className="col-span-2" />
